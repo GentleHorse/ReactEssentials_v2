225058667e51533527f74f65f3fe68f99302f40f
GentleHorse <anch.abc.t@gmail.com>
2025-01-04 23:37:36 +0100

    Add the rough contents to App

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Vite + React
+# React Essentials v2
 
-## Installation
+## 0. Set up
 
 Using Vite to create a application. You need to select 'React' framework and 'JavaScript' variant.
 
@@ -11,9 +11,12 @@ Using Vite to create a application. You need to select 'React' framework and 'Ja
 
 <br />
 
-**_If you clone or download this depo and start a project based on it, you can skip above command._**
+**_If you start a project based on the first commit files of this depo, you can skip above command._**
 
-## Install depedencies & run the server
+<br  />
+
+
+Install dependencies.
 
 ```
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,157 @@
 export default function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <br />
-      <p>Test screen</p>
+      <div>
+        <h1>Vite + React</h1>
+      </div>
+
+      <div>
+        <h1>Installation</h1>
+      </div>
+
+      <div>
+        <h1>Basics</h1>
+        <ul>
+          <li>
+            <p>useEffect</p>
+          </li>
+          <li>
+            <p>useState</p>
+          </li>
+          <li>
+            <p>useRef</p>
+          </li>
+          <li>
+            <p>props</p>
+          </li>
+          <li>
+            <p>useCallback</p>
+          </li>
+          <li>
+            <p>contextAPI</p>
+          </li>
+          <li>
+            <p>useReducer</p>
+          </li>
+          <li>
+            <p>useMemo</p>
+          </li>
+          <li>
+            <p>Built-in React components</p>
+          </li>
+          <li>
+            <p>Tips</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Styling</h1>
+        <ul>
+          <li>
+            <p>CSS styling</p>
+          </li>
+          <li>
+            <p>styled components</p>
+          </li>
+          <li>
+            <p>Tailwind CSS</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Events</h1>
+        <ul>
+          <li>
+            <p>Mouse events</p>
+          </li>
+          <li>
+            <p>Drag events</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Advanced</h1>
+        <ul>
+          <li>
+            <p>Class based components</p>
+          </li>
+          <li>
+            <p>Error handling</p>
+          </li>
+          <li>
+            <p>Custom hooks</p>
+          </li>
+          <li>
+            <p>Forms and user inputs</p>
+          </li>
+          <li>
+            <p>Reset user inputs</p>
+          </li>
+          <li>
+            <p>Input validation</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Redux</h1>
+        <ul>
+          <li>
+            <p>Redux intro</p>
+          </li>
+          <li>
+            <p>Redux with React - 1</p>
+          </li>
+          <li>
+            <p>Redux with React - 2</p>
+          </li>
+          <li>
+            <p>Redux Toolkit</p>
+          </li>
+          <li>
+            <p>Redux advanced</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>React router</h1>
+        <ul>
+          <li>
+            <p>React router - 1</p>
+          </li>
+          <li>
+            <p>React router - 2</p>
+          </li>
+          <li>
+            <p>React router - 3</p>
+          </li>
+          <li>
+            <p>React router - 4</p>
+          </li>
+          <li>
+            <p>React router - 5</p>
+          </li>
+          <li>
+            <p>React router - 6</p>
+          </li>
+          <li>
+            <p>React router - 7</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Authentication</h1>
+        <ul>
+          <li>
+            <p>Authentication - 1</p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
