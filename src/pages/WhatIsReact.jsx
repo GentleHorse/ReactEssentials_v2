e5ecdc1a30ba9a448ec47cc13b3f553357fc1ab7
GentleHorse <anch.abc.t@gmail.com>
2025-01-05 22:44:56 +0100

    Add core concepts to the what is react page

--- src/pages/WhatIsReact.jsx
+++ src/pages/WhatIsReact.jsx
@@ -1,5 +1,60 @@
+const CORE_CONCEPTS = [
+  {
+    id: "cc1",
+    title: "Components",
+    imgPath: "/images/components.png",
+    texts:
+      "The core UI building block - compose the user interface by combining multiple components.",
+  },
+  {
+    id: "cc2",
+    title: "JSX",
+    imgPath: "/images/jsx.png",
+    texts:
+      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+  },
+  {
+    id: "cc3",
+    title: "Props",
+    imgPath: "/images/props.png",
+    texts:
+      "Make components configurable (and therefore reusable) by passing input data to them.",
+  },
+  {
+    id: "cc4",
+    title: "State",
+    imgPath: "/images/state.png",
+    texts:
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+  },
+];
+
 export default function WhatIsReactPage() {
-    return <>
-        <h1>What is React?</h1>
+  return (
+    <>
+      <h1 className="font-poiretOneRegular text-center text-6xl mx-6 mt-8 mb-14">
+        What is React?
+      </h1>
+
+      <section className="w-3/4 mx-auto flex flex-col gap-6">
+        {CORE_CONCEPTS.map((concept) => (
+          <CoreConceptWrapper key={concept.id} coreConcept={concept} />
+        ))}
+      </section>
     </>
+  );
+}
+
+function CoreConceptWrapper({ coreConcept }) {
+  return (
+    <div className="flex flex-row items-center p-5 rounded-2xl backdrop-blur-md bg-[#FFFFFF]/15">
+      <img src={coreConcept.imgPath} className="w-[300px] h-[300px]" />
+      <div>
+        <h1 className="font-roboto text-xl m-2">{coreConcept.title}</h1>
+        <p className="font-montserrat text-stone-600 mx-4 mb-4">
+          {coreConcept.texts}
+        </p>
+      </div>
+    </div>
+  );
 }
