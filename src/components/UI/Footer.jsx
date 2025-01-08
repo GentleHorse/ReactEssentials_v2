90cd8993f0c054458d54f182716c02fb20c1db16
GentleHorse <anch.abc.t@gmail.com>
2025-01-08 19:33:44 +0100

    Added the 'useState' sub topic contents in the basics page

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <section className="bottom-0 mb-2 w-full text-center">
+      <section className="bottom-0 mt-10 mb-2 w-full text-center">
         <p className="font-sans text-slate-400 text-sm">
           &copy; Copyright 2025, All Rights Reserved by Toshihito Endo
         </p>
--- src/pages/Basics.jsx
+++ src/pages/Basics.jsx
@@ -227,35 +227,257 @@ const BASICS_TOPICS_ARRAY = [
       },
       {
         id: "sbtp3",
-        title: "Two-way binding",
-        text: `Two-way binding gives components in your application a way to share data. Use two-way binding to listen for events and update values simultaneously between parent and child components.`,
+        title: "Updating state based on old state",
+        text: `Pass a function to the state updating function. This function will automatically be called by React and will receive the guranteed latest state value. You need to call a function inside the state updating function(= setIsToggle()) because state WILL NOT INSTANTLY BE UPDATED since React set scheduling. Thus, 'setIsToggle(!isToggle)' is wrong!!`,
         code: `
+      export default function App() {
+        const [isToggle, setIsToggle] = React.useState(false);
         
+        const toggleHandler = () => {
+            setIsToggle(isToggle => !isToggle);
+        }
+        
+        return (
+            <div>
+                <p className={isToggle ? "active" : ""}>Style me!</p>
+                <button onClick={toggleHandler}>Toggle style</button>
+            </div>
+        );
+      }
         `,
       },
       {
         id: "sbtp4",
-        title: "",
-        text: ``,
-        code: ``,
+        title: "Two-way binding",
+        text: `Two-way binding gives components in your application a way to share data. Use two-way binding to listen for events and update values simultaneously between parent and child components.`,
+        code: `
+      function App() {
+          const [feedback, setFeedback] = React.useState();
+
+          const feedbackHandler = (event) => {
+              setFeedback(event.target.value);
+          }
+          
+        return (
+          <>
+
+              ....
+
+                <textarea value={feedback} onChange={feedbackHandler}/>
+              
+              ....  
+
+          </>
+        );
+      }
+
+      export default App
+        `,
       },
       {
         id: "sbtp5",
-        title: "",
-        text: ``,
-        code: ``,
+        title: "Update object and array state immutably",
+        text: `Objects & arrays (which technically are objects) are reference values in JavaScript. Therefore you should not mutate them directly, instead create a (deep) copy first! In below example, inside the selectSquarehandler function, ‘prevGameBoard’ is not mutated instead using its copy ‘updatedBoard’ for updating the state.`,
+        code: `
+      const initialGameBoard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ];
+
+      export default function GameBoard() {
+        const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+        const selectSquareHandler = (rowIndex, colIndex) => {
+          setGameBoard((prevGameBoard) => {
+            const updatedBoard = [
+              ...prevGameBoard.map((innerArray) => [...innerArray]),
+            ];
+            updatedBoard[rowIndex][colIndex] = "X";
+
+            return updatedBoard;
+          });
+        };
+
+        return (
+          <> ... </>
+        );
+      }
+        `,
       },
       {
         id: "sbtp6",
-        title: "",
-        text: ``,
-        code: ``,
+        title: "Lifting state up",
+        text: `Lift the state up to the closest ancestor component (= App.jsx) that has access to all components (= Player.jsx, GameBoard.jsx) that need to work with that state (= activePlayer). Player.jsx uses that state for css styling and GameBoard.jsx does for game logic (switching symbols between 'X' and 'O' + fetching current active player symbol).`,
+        code: `
+        App.jsx --------------------------------------------------
+
+        const [activePlayer, setActivePlayer] = useState("X");
+
+          const selectSquareHandler = () => {
+            setActivePlayer((currentActivePlayer) =>
+              currentActivePlayer === "X" ? "O" : "X"
+            );
+          };
+
+          return (
+            <main>
+              <div id="game-container">
+                <ol id="players" className="highlight-player">
+                  <Player
+                    initialName="Player-1"
+                    symbol="X"
+                    isActive={activePlayer === "X"}
+                  />
+                  <Player
+                    initialName="Player-2"
+                    symbol="O"
+                    isActive={activePlayer === "O"}
+                  />
+                </ol>
+                <GameBoard
+                  onSelectSquare={selectSquareHandler}
+                  activePlayerSymbol={activePlayer}
+                />
+              </div>
+              LOG
+            </main>
+
+        Player.jsx -----------------------------------------------
+
+        return (
+          <li className={isActive ? "active" : undefined}>
+            <span className="player">
+              {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={editNameHandler}>
+              {isEditing ? "Save" : "Edit"}
+            </button>
+          </li>
+        );
+
+        GameBoard.jsx --------------------------------------------
+
+        export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+          const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+          const selectSquareHandler = (rowIndex, colIndex) => {
+            setGameBoard((prevGameBoard) => {
+              const updatedBoard = [
+                ...prevGameBoard.map((innerArray) => [...innerArray]),
+              ];
+              updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+
+              return updatedBoard;
+            });
+
+            onSelectSquare();
+          };
+
+          return (
+            <ol id="game-board">
+              {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                  <ol>
+                    {row.map((playerSymbol, colIndex) => (
+                      <li key={colIndex}>
+                        <button onClick={() => selectSquareHandler(rowIndex, colIndex)}>
+                          {playerSymbol}
+                        </button>
+                      </li>
+                    ))}
+                  </ol>
+                </li>
+              ))}
+            </ol>
+          );
+        }
+        `,
       },
       {
         id: "sbtp7",
-        title: "",
-        text: ``,
-        code: ``,
+        title: "One object state and one change handler",
+        text: `There's one object state which contains multiple 'key-value' pairs and one change handler, and if you want to update only one 'key-value' pair in correspondence with one of the identifiers, here's the answer.`,
+        code: `
+      /**
+       * Object State
+       */
+        const [userInput, setUserInput] = useState({
+            initialInvestment: 10000,
+            annualInvestment: 1200,
+            expectedReturn: 6,
+            duration: 10,
+          });
+        
+      /**
+       * Change Handler
+       */
+        const changeHandler = (inputIdentifier, newValue) => {
+          setUserInput((prevUseInput) => {
+            return {
+              ...prevUseInput,
+              [inputIdentifier]: +newValue,  // "+" > string to number
+            };
+          });
+        };
+        
+      /**
+       * JSX Renderer
+       */
+        return (
+          <section id="user-input">
+            <div className="input-group">
+              <p>
+                <label>intial investment</label>
+                <input
+                  type="number"
+                  required
+                  value={userInput.initialInvestment}
+                  onChange={(event) =>
+                    changeHandler("initialInvestment", event.target.value)
+                  }
+                />
+              </p>
+              <p>
+                <label>annual investment</label>
+                <input
+                  type="number"
+                  required
+                  value={userInput.annualInvestment}
+                  onChange={(event) =>
+                    changeHandler("annualInvestment", event.target.value)
+                  }
+                />
+              </p>
+            </div>
+            <div className="input-group">
+              <p>
+                <label>expected return</label>
+                <input
+                  type="number"
+                  required
+                  value={userInput.expectedReturn}
+                  onChange={(event) =>
+                    changeHandler("expectedReturn", event.target.value)
+                  }
+                />
+              </p>
+              <p>
+                <label>duration</label>
+                <input
+                  type="number"
+                  required
+                  value={userInput.duration}
+                  onChange={(event) =>
+                    changeHandler("duration", event.target.value)
+                  }
+                />
+              </p>
+            </div>
+          </section>
+        );
+        `,
       },
     ]
   },
