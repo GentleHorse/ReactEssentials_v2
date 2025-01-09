2d0de2cde80749b29b853d241852f3f2fef1e190
GentleHorse <anch.abc.t@gmail.com>
2025-01-10 00:32:17 +0100

    Added the 'useCallback' sub topic contents in the basics page

--- src/pages/Basics.jsx
+++ src/pages/Basics.jsx
@@ -852,9 +852,52 @@ const BASICS_TOPICS_ARRAY = [
     subTopics: [
       {
         id: "sbtp1",
-        title: "",
-        text: ``,
-        code: ``,
+        title: "What is it?",
+        text: `useCallback is a React Hook that lets you cache a function definition between re-renders. Normally function objects get automatically newly re-created every component(s) render cycles, but useCallback prevents this automatic re-creation. Using useCallback hook is a good practice for preventing undesired infinite loops.`,
+        code: `
+      const memoizedCallback = useCallback(
+        () => {
+          doSomething(a, b);
+        },
+        [a, b],
+      );        
+      `,
+      },
+      {
+        id: "sbtp2",
+        title: "Use with memo()",
+        text: `When you use memo() and pass functions as dependencies, you should wrap these functions with useCallback() to prevent creating function newly otherwise memo() cannot prevent re-renderinng components. (= because new creation means passing new props to child components).`,
+        code: `
+      const incrementHandler = useCallback(function incrementHandler() {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, []);
+
+      return (
+
+          ...
+
+            <IconButton icon={PlusIcon} onClick={incrementHandler}>
+              Increment
+            </IconButton>
+
+
+      ----------------------------------------------------------------------
+
+      import { memo } from "react";
+
+      const IconButton = memo(function IconButton({ children, icon, ...props }) {
+
+        return (
+          <button {...props} className="button">
+            
+            ...
+
+          </button>
+        );
+      });
+
+      export default IconButton;        
+      `,
       },
     ],
   },
