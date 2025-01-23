94477b364c3cdf636eb8c7d6ed38d0ce71926eb0
GentleHorse <anch.abc.t@gmail.com>
2025-01-23 22:34:48 +0100

    Put the mouse events and drag events data into topic data file

--- src/topics-data.js
+++ src/topics-data.js
@@ -2017,3 +2017,111 @@ export const AUTHENTICATION_TOPICS_ARRAY = [
     ],
   },
 ];
+
+export const MOUSE_EVENTS = [
+  {
+    id: "e1",
+    title: "onCLick",
+    link: "https://www.w3schools.com/jsref/event_onclick.asp",
+    texts: "Clicks on an element",
+  },
+  {
+    id: "e2",
+    title: "onContextMenu",
+    link: "https://www.w3schools.com/jsref/event_oncontextmenu.asp",
+    texts: "Right-clicks on an element",
+  },
+  {
+    id: "e3",
+    title: "onDoubleClick",
+    link: "https://www.w3schools.com/jsref/event_ondblclick.asp",
+    texts: "Double-clicks on an element",
+  },
+  {
+    id: "e4",
+    title: "onMouseDown",
+    link: "https://www.w3schools.com/jsref/event_onmousedown.asp",
+    texts: "A mouse button is pressed over an element",
+  },
+  {
+    id: "e5",
+    title: "onMouseEnter",
+    link: "https://www.w3schools.com/jsref/event_onmouseenter.asp",
+    texts: "The pointer is moved onto an element",
+  },
+  {
+    id: "e6",
+    title: "onMouseLeave",
+    link: "https://www.w3schools.com/jsref/event_onmouseleave.asp",
+    texts: "The pointer is moved out of an element",
+  },
+  {
+    id: "e7",
+    title: "onMouseMove",
+    link: "https://www.w3schools.com/jsref/event_onmousemove.asp",
+    texts: "The pointer is moving over an element",
+  },
+  {
+    id: "e8",
+    title: "onMouseOut",
+    link: "https://www.w3schools.com/jsref/event_onmouseout.asp",
+    texts: "The mouse pointer moves out of an element",
+  },
+  {
+    id: "e9",
+    title: "onMouseOver",
+    link: "https://www.w3schools.com/jsref/event_onmouseover.asp",
+    texts: "The mouse pointer is moved over an element",
+  },
+  {
+    id: "e10",
+    title: "onMouseUp",
+    link: "https://www.w3schools.com/jsref/event_onmouseup.asp",
+    texts: "The mouse button is released over an element",
+  },
+];
+
+export const DRAG_EVENTS = [
+  {
+    id: "e1",
+    title: "onDrag",
+    link: "https://www.w3schools.com/jsref/event_ondrag.asp",
+    texts: "An element is being dragged",
+  },
+  {
+    id: "e2",
+    title: "onDragStart",
+    link: "https://www.w3schools.com/jsref/event_ondragstart.asp",
+    texts: "The user starts to drag an element",
+  },
+  {
+    id: "e3",
+    title: "onDragEnd",
+    link: "https://www.w3schools.com/jsref/event_ondragend.asp",
+    texts: "Finished dragging an element",
+  },
+  {
+    id: "e4",
+    title: "onDragEnter",
+    link: "https://www.w3schools.com/jsref/event_ondragenter.asp",
+    texts: "A dragged element enters the drop target",
+  },
+  {
+    id: "e5",
+    title: "onDragLeave",
+    link: "https://www.w3schools.com/jsref/event_ondragleave.asp",
+    texts: "A dragged element leaves the drop target",
+  },
+  {
+    id: "e6",
+    title: "onDragOver",
+    link: "https://www.w3schools.com/jsref/event_ondragover.asp",
+    texts: "A dragged element is over the drop target",
+  },
+  {
+    id: "e7",
+    title: "onDrop",
+    link: "https://www.w3schools.com/jsref/event_ondrop.asp",
+    texts: "A dragged element is dropped on the target",
+  },
+];
