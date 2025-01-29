29287c1a369122975e1a1cfdff779d6cad04c65d
GentleHorse <anch.abc.t@gmail.com>
2025-01-29 21:23:25 +0100

    Added the table of contents to the styling page

--- src/pages/Styling.jsx
+++ src/pages/Styling.jsx
@@ -4,6 +4,7 @@ import TopicWrapper from "../components/UI/TopicWrapper.jsx";
 import { STYLING_TOPICS_ARRAY } from "../topics-data.js";
 import Header from "../components/UI/Header.jsx";
 import PageTitle from "../components/UI/PageTitle.jsx";
+import TableOfContents from "../components/UI/TableOfContents.jsx";
 
 export default function StylingPage() {
   const [topicId, setTopicId] = useState("tp1");
@@ -35,9 +36,16 @@ export default function StylingPage() {
           />
         </div>
 
-        <ul className="col-span-2 max-w-[600px] lg:max-w-[1200px] mr-10 md:mr-12 lg:mr-20">
-          <TopicWrapper topics={STYLING_TOPICS_ARRAY[topicIndex].subTopics} />
-        </ul>
+        <div className="col-span-2 max-w-[600px] lg:max-w-[1200px] mr-10 md:mr-12 lg:mr-20">
+          <TableOfContents
+            topics={STYLING_TOPICS_ARRAY[topicIndex].subTopics}
+            textsPosition="left"
+          />
+
+          <ul>
+            <TopicWrapper topics={STYLING_TOPICS_ARRAY[topicIndex].subTopics} />
+          </ul>
+        </div>
       </section>
     </>
   );
