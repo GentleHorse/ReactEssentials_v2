2674ef7bcb0fa7033817be39067808ac0b917a83
GentleHorse <anch.abc.t@gmail.com>
2025-01-29 21:27:41 +0100

    Header component - scroll back to top when router change to home

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -4,7 +4,12 @@ export default function Header() {
   return (
     <>
       <section className="fixed my-auto z-20 h-[20vh]">
-        <Link to="/">
+        <Link
+          to="/"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
           <img
             src="/icons/react-mono.svg"
             className="mx-6 my-[5vh] w-[10vh] h-[10vh] hover:animate-tilt"
