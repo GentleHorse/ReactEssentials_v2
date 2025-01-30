cd73286825a04ef856e75bc2c25fbac7f9115d3c
GentleHorse <anch.abc.t@gmail.com>
2025-01-30 15:04:06 +0100

    Added contents to the authentication page

--- src/topics-data.js
+++ src/topics-data.js
@@ -5339,15 +5339,17 @@ export const REACT_ROUTER_TOPICS_ARRAY = [
 export const AUTHENTICATION_TOPICS_ARRAY = [
   {
     id: "tp1",
-    title: "",
+    title: "Intro",
     subTopics: [
       {
         id: "sbtp1",
-        title: "",
-        text: ``,
-        code: `
-  
-        `,
+        title: "Server-side Sessions",
+        text: `One of the way to authenticate a user is using “Server-side Sessions”. 1) The server stores the unique identifier and sends the same identifier to the client.  2) The client sends the identifier along with requests to protected resources. 3) The server can then check if the identifier which is previously issued by server to the client is valied. Server-side Session is the great way of authentications, however it requires the tight “coupling” between backend & frontend.`,
+      },
+      {
+        id: "sbtp2",
+        title: "Authentication Tokens",
+        text: `Another way of authentication is using “Authentication Tokens”.  Most React apps are SPAs (Single Page Applications) that are served by a server that’s “decoupled from the backend”. The SPA “handles routing on the client side” and only talks to the backend in case there needs data (or needs data to change).  In that case, “Authentication Tokens” is coming to play.  How is works is: 1) create (but not store) the “permission” token on the server and send it to the client, 2) the client attaches the token to future requests for protected resources, 3) the server can then verify the attached token.`,
       },
     ],
   },
