2c9d93942223d06d61026045a7102506956e91f8
GentleHorse <anch.abc.t@gmail.com>
2025-02-04 18:24:49 +0100

    Added content to the react router page

--- src/topics-data.js
+++ src/topics-data.js
@@ -5211,7 +5211,7 @@ export const REACT_ROUTER_TOPICS_ARRAY = [
   },
   {
     id: "tp6",
-    title: "Advanced",
+    title: "Bonus",
     subTopics: [
       {
         id: "sbtp1",
@@ -5434,6 +5434,92 @@ export const REACT_ROUTER_TOPICS_ARRAY = [
           </>
         );
       }
+        `,
+      },
+      {
+        id: "sbtp6",
+        title: "Reusable modal for react router",
+        text: `Here is one of the best practices of modal component.`,
+        code: `
+      // index.html ------------------------------------------------------------------------
+
+          <div id="modal"></div>
+          
+      // index.css -------------------------------------------------------------------------
+
+      .modal {
+        margin: 0;
+        padding: 2rem;
+        position: fixed;
+        top: 10vh;
+        left: calc(50% - 15rem);
+        width: 30rem;
+        max-height: 80vh;
+        background: #e2e5eb;
+        border: none;
+        border-radius: 6px;
+        z-index: 100;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        animation: slide-down-fade-in 300ms ease-out forwards;
+      }
+
+      .modal::backdrop {
+        height: 100vh;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+        left: 0;
+      }
+
+      @keyframes slide-down-fade-in {
+        from {
+          opacity: 0;
+          transform: translateY(-3rem);
+        }
+        to {
+          opacity: 1;
+          transform: translateY(0);
+        }
+      }
+
+      // Modal.jsx -------------------------------------------------------------------------
+
+      import { useEffect, useRef } from 'react';
+      import { createPortal } from 'react-dom';
+
+      export default function Modal({ children, onClose }) {
+        const dialog = useRef();
+
+        useEffect(() => {
+          // Using useEffect to sync the Modal component with the DOM Dialog API
+          // This code will open the native <dialog> via it's built-in API 
+          // whenever the <Modal> component is rendered
+          const modal = dialog.current;
+          modal.showModal();
+
+          return () => {
+            modal.close(); // needed to avoid error being thrown
+          };
+        }, []);
+
+        return createPortal(
+          <dialog className="modal" ref={dialog} onClose={onClose}>
+            {children}
+          </dialog>,
+          document.getElementById('modal')
+        );
+      }
+
+
+      // SomeComponent.jsx -----------------------------------------------------------------
+
+          <Modal onClose={() => navigate("../")}> .... </Modal>
+
+          // When the modal closes, it brings back to the one level higher route	
         `,
       },
     ],
