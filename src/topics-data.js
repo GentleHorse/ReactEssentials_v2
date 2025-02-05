439faa0f1467f694b078585aacf4d247724090c0
GentleHorse <anch.abc.t@gmail.com>
2025-02-05 15:06:07 +0100

    Added the contents to the basics page - tips section

--- src/topics-data.js
+++ src/topics-data.js
@@ -1530,6 +1530,46 @@ export const BASICS_TOPICS_ARRAY = [
       export default SomeComponent;
       `,
       },
+      {
+        id: "sbtp3",
+        title: "Outsourcing images import",
+        text: `If you need to import multiple images into files for multiple times, it’s redundant to import at each time. So it’s a good practice to create one import file and use it.`,
+        code: `
+      // File structure
+
+      // src > assets > constructing.png
+      // src > assets > cooking.png
+      // src > assets > family-time.png
+      // src > assets > playing-guitar.png
+      // src > assets > romantic-walk.png
+      // src > assets > working-on-computer.png
+      // src > assets > working-out.png
+
+      // src > assets > images.js
+
+      import constructingImg from '../assets/constructing.png';
+      import cookingImg from '../assets/cooking.png';
+      import familyTimeImg from '../assets/family-time.png';
+      import playingGuitarImg from '../assets/playing-guitar.png';
+      import romanticWalkImg from '../assets/romantic-walk.png';
+      import workingOnComputerImg from '../assets/working-on-computer.png';
+      import workingOutImg from '../assets/working-out.png';
+
+      export default [
+        { src: constructingImg, alt: 'Person working on some furniture.' },
+        { src: cookingImg, alt: 'Person cooking a meal.' },
+        { src: familyTimeImg, alt: 'Family spending time together.' },
+        { src: playingGuitarImg, alt: 'Person playing the guitar.' },
+        { src: romanticWalkImg, alt: 'Couple walking together in the moonshine.' },
+        { src: workingOnComputerImg, alt: 'Person doing work on a computer.' },
+        { src: workingOutImg, alt: 'Person working out.' },
+      ];
+
+      // When you want to import the above image array (for instance)
+
+      import images from '../assets/images.js';
+      `,
+      },
     ],
   },
 ];
