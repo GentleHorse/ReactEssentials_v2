eb74899e0781ce44984012123d9e7ff1b95ff9ad
GentleHorse <anch.abc.t@gmail.com>
2025-02-07 08:59:33 +0100

    Animated the scrolled to top button

--- src/components/UI/ScrollToTopButton.jsx
+++ src/components/UI/ScrollToTopButton.jsx
@@ -1,33 +1,36 @@
-export default function ScrollToTopButton({buttonPosition="right"}) {
-  
-  let buttonPositionClassName = "flex flex-col"; 
-  
-  if(buttonPosition==="center"){
-    buttonPositionClassName += " items-center"
+import { motion } from "framer-motion";
+
+export default function ScrollToTopButton({ buttonPosition = "right" }) {
+  let buttonPositionClassName = "flex flex-col";
+
+  if (buttonPosition === "center") {
+    buttonPositionClassName += " items-center";
   }
 
-  if(buttonPosition==="left"){
-    buttonPositionClassName += " items-start ml-8"
+  if (buttonPosition === "left") {
+    buttonPositionClassName += " items-start ml-8";
   }
 
-  if(buttonPosition==="right"){
-    buttonPositionClassName += " items-end mr-8"
+  if (buttonPosition === "right") {
+    buttonPositionClassName += " items-end mr-8";
   }
 
   return (
     <section className="fixed bottom-8 w-full">
       <div className={buttonPositionClassName}>
-        <button
-          className="w-28 h-28 p-2 
-          text-sm text-[#1C1C1C]/45 hover:text-[#1C1C1C]/85 hover:font-bold 
+        <motion.button
+          className="w-28 h-28 p-2
+          text-sm text-[#1C1C1C]/45 hover:text-[#1C1C1C]/85 hover:font-bold
           rounded-full backdrop-blur-sm bg-[#FFFFFF]/15 hover:bg-[#FFFFFF]/85
           border-2 border-[#1C1C1C]/15"
           onClick={() => {
             window.scrollTo(0, 0);
           }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 250 }}
         >
           Scroll to top
-        </button>
+        </motion.button>
       </div>
     </section>
   );
