0e1e09892302aa21d018f8a436050f055e178abd
GentleHorse <anch.abc.t@gmail.com>
2025-02-08 14:26:02 +0100

    Animated text and background color of the side bar component

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,25 +1,28 @@
+import { motion } from "framer-motion";
+
 export default function SideBar({ topicsArray, setTopic, topicId }) {
   return (
     <aside className="fixed w-1/4 px-8">
       <ul className="mt-8">
         {topicsArray.map((topic) => {
-          let cssClasses =
-            "w-full text-left text-xl font-montserrat px-2 py-1 rounded-sm my-1 hover:text-[#FFB11B]/75 hover:bg-stone-800";
+          let cssClasses = "w-full text-left text-xl font-montserrat px-2 py-1 rounded-sm my-1";
 
-          if (topic.id === topicId) {
-            cssClasses += " bg-stone-800 text-[#FFB11B]/75";
-          } else {
-            cssClasses += " text-stone-800";
-          }
+          // if (topic.id === topicId) {
+          //   cssClasses += " bg-stone-800 text-[#FFB11B]/75";
+          // } else {
+          //   cssClasses += " text-stone-800";
+          // }
 
           return (
             <li key={topic.id}>
-              <button
+              <motion.button
                 className={cssClasses}
                 onClick={() => setTopic(topic)}
+                animate={{color: (topic.id === topicId) ? "#FFB11B85" : "#373C38", backgroundColor: (topic.id === topicId) ? "#1C1C1C" : "#FFFFFF00"}}
+                whileHover={{ color: "#FFB11B85", backgroundColor: "#1C1C1C" }}
               >
                 {topic.title}
-              </button>
+              </motion.button>
             </li>
           );
         })}
